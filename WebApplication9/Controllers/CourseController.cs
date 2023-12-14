85b9b271cce0d1e9abc57b6436da6526c8d81448
elwirajaroc <elwira.jaroc@gmail.com>
2023-12-14 09:18:08 +0100

    kursy

---
+++ WebApplication9/Controllers/CourseController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using WebApplication9.Models;
+
+namespace WebApplication9.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        // GET: Course
+        public CourseController()
+        {
+            _context = new ApplicationDbContext(); // Inicjalizacja ApplicationDbContext
+        }
+        public ActionResult Courses()
+        {
+            // Logika pobierania danych dotyczących kursów
+
+            return View(); // Zwrócenie widoku z kursami
+        }
+        public ActionResult Index()
+        {
+            var courses = _context.Courses.ToList(); // Pobranie kursów z bazy danych
+            return View(courses); // Przekazanie kursów do widoku
+        }
+
+        [HttpPost]
+        public ActionResult Create(Course course)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Courses.Add(course); // Dodanie nowego kursu do bazy danych
+                _context.SaveChanges(); // Zapisanie zmian w bazie danych
+                return RedirectToAction("Index");
+            }
+            return View(course);
+        }
+    }
+}
